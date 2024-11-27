1dd5706c6e022098a19982e7e9929e337bd05c65
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-11-27 15:31:15 +0000

    Create get_sales_data function to get data string from user.

--- run.py
+++ run.py
@@ -13,8 +13,13 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
+def get_sales_data():
+    """
+    get sales figures from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("10,20,30,40,50,60\n")
+    data_str =input("Enter your data here: ")
+    print(f"The data provided is: {data_str}")
+get_sales_data() 
