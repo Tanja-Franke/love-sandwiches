dfea5aa1132bad5697e6d4520bf416354a66ad06
Tanja Franke <wwicxcut@students.codeinstitute.net>
2024-11-28 10:12:58 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -11,8 +11,8 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 CREDS = Credentials.from_service_account_file('creds.json')
-SCOPE_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 def get_sales_data():
     """
@@ -31,6 +31,7 @@ def get_sales_data():
         if validate_data(sales_data):
             print("Data is valid")
             break
+
     return sales_data
 
 def validate_data(values):
@@ -107,10 +108,24 @@ def get_last_5_entries_sales():
 
     columns = []
     for ind in range(1, 7):
-        columns = sales.col_values(ind)
-        columns.append(columns[-5:])
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
     return columns
-    
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data=[]
+
+    for column in data:
+        int_column=[int(num) for num in column]
+        average = sum(int_column)/ len(int_column)
+        stock_num= average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data   
 def main():
     """
     run all program functions
@@ -118,9 +133,12 @@ def main():
     data = get_sales_data() 
     sales_data =[int(num) for num in data]
     update_worksheet(sales_data, "sales")
-    new_surplus_data=calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+
     
 print("Welcome to Love Sandwich Data Automation")
-#main()
-sales_columns = get_last_5_entries_sales()
+main()
